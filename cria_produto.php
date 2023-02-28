953a62f6551a99263baec1e4c980d1c2496ac056
NathanMaced <nathanmacedo02@gmail.com>
2023-02-27 21:51:33 -0300

    Aula Orientação

---
+++ cria_produto.php
@@ -0,0 +1,9 @@
+<?php
+
+include_once 'produto.php';
+
+$valor = new Produto();
+
+$valor-> Codigo = 5932;
+
+?>
---
+++ produto.php
@@ -0,0 +1,11 @@
+<?php 
+
+class Produto
+{
+    var $Codigo;
+    var $Descricao;
+    var $Preco;
+    var $Quantidade;
+}
+
+?>
